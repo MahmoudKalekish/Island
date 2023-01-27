a849b1069ff5dc5182800b298e9d7d404d908b3b
Mahmoud Kalekish <mahmoudkalekish18@gmail.com>
2023-01-27 19:24:21 +0200

    MyIsland

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import island_01 from "./assets/4.jpg";
+import island_02 from "./assets/5.jpg";
+import island_03 from "./assets/6.jpg";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import PlaceToVisit from './components/PlaceToVisit';
+import Footer from "./components/Footer";
+import Loading from "./components/Loading";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     minHeight: '100vh',
+//     backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/4.jpg'})`,
+//     backgroundRepeat: 'no-repeat',
+//     backgroundSize: 'cover',
+   
+//   },
+// }));
 
 function App() {
+  const navbarLinks = [
+    { url: "#", title: "Home" },
+    { url: "#", title: "Trips" },
+    { url: "#", title: "Rewards" },
+  ];
+
+  // const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={island_01} />
+      <Slider
+        imageSrc={island_02}
+        title={"Be an explorer."}
+        subtitle={
+          "Our platform offers a wide variety of unique travel locations!"
+        }
+      />
+      <Slider
+        imageSrc={island_03}
+        title={"Memories for a lifetime."}
+        subtitle={"Your dream vacation is only a few clicks away."}
+        flipped={true}
+      />
+
+<CssBaseline />
+      <Header />
+      {/* <Loading/> */}
+      <PlaceToVisit />
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+     <section>
+    <footer>
+        <div class="wave wave1"></div>
+        <div class="wave wave2"></div>
+        <div class="wave wave3"></div>
+        <div class="wave wave4"></div>
+    </footer>
+   </section> 
+    </div>
+  )
+}
+
+export default Footer
